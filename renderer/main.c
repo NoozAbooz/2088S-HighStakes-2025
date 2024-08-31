f45a75034619609f444c3e04a350cd6615b043fd
Michael Zheng <zhengm58@gmail.com>
2024-08-31 10:58:19 -0600

    finish lvgl renderer

--- renderer/main.c
+++ renderer/main.c
@@ -89,9 +89,66 @@ int main(int argc, char **argv)
   hal_init();
 
 red_slider = create_slider(lv_palette_main(LV_PALETTE_RED));
+green_slider = create_slider(lv_palette_main(LV_PALETTE_GREEN));
+blue_slider = create_slider(lv_palette_main(LV_PALETTE_BLUE));
+
+lv_slider_set_value(red_slider, 24, LV_ANIM_OFF);
+lv_slider_set_value(green_slider, 24, LV_ANIM_OFF);
+lv_slider_set_value(blue_slider, 0, LV_ANIM_OFF);
+
 lv_obj_align(red_slider, LV_ALIGN_LEFT_MID, 25, 120);
 lv_obj_align_to(green_slider, red_slider, LV_ALIGN_OUT_RIGHT_MID, 25, 0);
 lv_obj_align_to(blue_slider, green_slider, LV_ALIGN_OUT_RIGHT_MID, 25, 0);
+    
+
+  lv_obj_t *screen = lv_scr_act();
+  lv_obj_t *view_cont = lv_obj_create(screen);
+  lv_obj_clear_flag(view_cont, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_set_size(view_cont, 480, 240);
+	lv_obj_align(view_cont, LV_ALIGN_TOP_LEFT, 0, 0);
+  lv_obj_set_style_bg_color(view_cont, lv_color_black(), LV_PART_MAIN);
+
+  static lv_style_t style_title;
+  lv_style_init(&style_title);
+  lv_style_set_text_font(&style_title, &lv_font_montserrat_20); 
+  lv_style_set_text_decor(&style_title, LV_TEXT_DECOR_UNDERLINE);
+
+  static lv_style_t style_data;
+  lv_style_init(&style_data);
+  lv_style_set_text_font(&style_data, &lv_font_montserrat_16); 
+
+  lv_obj_t* label = lv_label_create(view_cont);
+  lv_label_set_recolor(label, true); 
+  lv_label_set_text(label, "#ffffff 210P Odom");
+  lv_obj_align(label, LV_ALIGN_CENTER, 120, -80);
+  lv_obj_add_style(label, &style_title, 0);
+
+  lv_obj_t* label1 = lv_label_create(view_cont);
+  lv_label_set_recolor(label1, true); 
+  lv_obj_align(label1, LV_ALIGN_CENTER, 60, -40);
+  lv_obj_add_style(label1, &style_data, 0);
+
+  lv_obj_t* label2 = lv_label_create(view_cont);
+  lv_label_set_recolor(label2, true); 
+  lv_obj_align(label2, LV_ALIGN_CENTER, 60, 0);
+  lv_obj_add_style(label2, &style_data, 0);
+
+  lv_obj_t* label3 = lv_label_create(view_cont);
+  lv_label_set_recolor(label3, true); 
+  lv_obj_align(label3, LV_ALIGN_CENTER, 60, 40);
+  lv_obj_add_style(label3, &style_data, 0);
+
+  LV_IMG_DECLARE(field);
+  lv_obj_t * img = lv_img_create(view_cont);
+  lv_img_set_src(img, &field);
+  lv_obj_align(img, LV_ALIGN_CENTER, -120, 0);
+  lv_img_set_zoom(img, 84);
+
+  LV_IMG_DECLARE(arrow);
+  lv_obj_t * img2 = lv_img_create(view_cont);
+  lv_img_set_src(img2, &arrow);
+  lv_obj_align(img2, LV_ALIGN_CENTER, 0, 0);
+  lv_img_set_zoom(img2, 64);
 
   int horizontal_offset = -120;
   slider_event_cb(LV_EVENT_VALUE_CHANGED);
@@ -102,6 +159,17 @@ lv_obj_align_to(blue_slider, green_slider, LV_ALIGN_OUT_RIGHT_MID, 25, 0);
     lv_img_set_angle(img2, (theta * 10));
     create_marker(view_cont, horizontal_pos, vertical_pos);
     lv_obj_move_foreground(img2);
+    
+    char buf[32];
+    snprintf(buf, 32, "#ffffff X: %d", lv_slider_get_value(red_slider));
+    lv_label_set_text(label1, buf);
+
+    snprintf(buf, 32, "#ffffff Y: %d", lv_slider_get_value(green_slider));
+    lv_label_set_text(label2, buf);
+
+    snprintf(buf, 32, LV_SYMBOL_GPS "#ffffff : %d°", lv_slider_get_value(blue_slider));
+    lv_label_set_text(label3, buf);
+
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
     lv_timer_handler();
@@ -149,7 +217,40 @@ static void hal_init(void)
   lv_disp_set_theme(disp, th);
 
   lv_group_t * g = lv_group_create();
-  lv_group_set_default(g);          /*Connect the image  object to the driver*/
+  lv_group_set_default(g);
+
+  /* Add the mouse as input device
+   * Use the 'mouse' driver which reads the PC's mouse*/
+  // mouse_init();
+  static lv_indev_drv_t indev_drv_1;
+  lv_indev_drv_init(&indev_drv_1); /*Basic initialization*/
+  indev_drv_1.type = LV_INDEV_TYPE_POINTER;
+
+  /*This function will be called periodically (by the library) to get the mouse position and state*/
+  indev_drv_1.read_cb = sdl_mouse_read;
+  lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
+
+  // keyboard_init();
+  static lv_indev_drv_t indev_drv_2;
+  lv_indev_drv_init(&indev_drv_2); /*Basic initialization*/
+  indev_drv_2.type = LV_INDEV_TYPE_KEYPAD;
+  indev_drv_2.read_cb = sdl_keyboard_read;
+  lv_indev_t *kb_indev = lv_indev_drv_register(&indev_drv_2);
+  lv_indev_set_group(kb_indev, g);
+  // mousewheel_init();
+  static lv_indev_drv_t indev_drv_3;
+  lv_indev_drv_init(&indev_drv_3); /*Basic initialization*/
+  indev_drv_3.type = LV_INDEV_TYPE_ENCODER;
+  indev_drv_3.read_cb = sdl_mousewheel_read;
+
+  lv_indev_t * enc_indev = lv_indev_drv_register(&indev_drv_3);
+  lv_indev_set_group(enc_indev, g);
+
+  /*Set a cursor for the mouse*/
+  LV_IMG_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+  lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
+  lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+  lv_indev_set_cursor(mouse_indev, cursor_obj);            /*Connect the image  object to the driver*/
 }
 
 /**
