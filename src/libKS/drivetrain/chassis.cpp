9fde56804c40e3aeb9610aaef39eac400bafbf5c
Michael Zheng <zhengm58@gmail.com>
2024-09-30 20:34:38 -0600

    ignore libks in docs

--- src/libKS/drivetrain/chassis.cpp
+++ src/libKS/drivetrain/chassis.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void moveRaw(int voltage, int time) {
+void ks::moveRaw(int voltage, int time) {
 	leftDrive.move_voltage(voltage);
 	rightDrive.move_voltage(voltage);
 
@@ -11,11 +11,11 @@ void moveRaw(int voltage, int time) {
 }
 
 // opcontrol
-double driveCurve(double input, double curve) {
+double ks::driveCurve(double input, double curve) {
     return (std::pow(2.718, -(curve / 10)) + std::pow(2.718, (std::abs(input) - 127) / 10) * (1 - std::pow(2.718, -(curve / 10)))) * input;
 }
 
-void arcadeDrive(int linCurve, int rotCurve, double turnScale) {
+void ks::arcadeDrive(int linCurve, int rotCurve, double turnScale) {
     // poll joystick input and convert to mv, then run through drivecurve function
     int power = ks::driveCurve(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y), linCurve);
     int turn = ks::driveCurve(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X), rotCurve);
