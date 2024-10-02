7195ea3695728fbf3894c669221834f83dfbd992
Michael Zheng <zhengm58@gmail.com>
2024-10-01 21:00:21 -0600

    revert py

--- scripts/prosToMinted.py
+++ scripts/prosToMinted.py
@@ -146,7 +146,6 @@ def main():
             # It is good convention to keep all your header files in include/
             # Sub directories within include/ will still be found
             if('api.h' in components
-            or ('include' and 'libKS') in components
             or ('include' and 'main.h') in components
             or ('include' and 'liblvgl') in components
             or ('include' and 'display') in components
