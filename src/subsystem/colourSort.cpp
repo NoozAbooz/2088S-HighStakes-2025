86d82446a283acda4bd3166e65e2f8997480c74c
Michael Zheng <zhengm58@gmail.com>
2024-10-25 14:38:34 -0600

    gruh

--- src/subsystem/colourSort.cpp
+++ src/subsystem/colourSort.cpp
@@ -1,6 +1,8 @@
 #include "main.h"
 
+bool sortToggle = true;
 bool lock = false;
+
 // Colour Sorter
 void raiseHoodTask() {
 	lock = true;
@@ -14,10 +16,15 @@ void raiseHoodTask() {
 }
 
 void refreshColourSort() {
-	if (alliance == "blue" && optical.get_hue() > 100 && optical.get_hue() < 200 && lock == false) {
+	if (alliance == "blue" && optical.get_hue() > 100 && optical.get_hue() < 200 && lock == false && sortToggle == true) {
 		raiseHoodTask();
 	}
-	if (alliance == "red" && optical.get_hue() > 15 && optical.get_hue() < 40 && lock == false) {
+	if (alliance == "red" && optical.get_hue() > 15 && optical.get_hue() < 40 && lock == false && sortToggle == true) {
 		raiseHoodTask();
 	}
+
+	if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
+    	sortToggle = !sortToggle;
+		optical.set_led_pwm((sortToggle == true ? 75 : 0)); // Turn off LED if not sorting
+    }
 }
