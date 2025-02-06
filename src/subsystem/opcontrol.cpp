5b20a6d9380055c2a11b8366c2b09a820e1c9605
Nooz <44128563+NoozAbooz@users.noreply.github.com>
2025-02-05 20:30:20 -0700

    elims auto

--- src/subsystem/opcontrol.cpp
+++ src/subsystem/opcontrol.cpp
@@ -5,6 +5,7 @@ void refreshIntake() {
 	if (intakeLock == false) {
 		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
 			intake.move_voltage(12000);
+			//console.printf("%.0lf\n", intake.get_efficiency());
 		} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
 			intake.move_voltage(-12000);
 		} else {
@@ -19,7 +20,6 @@ void refreshWallstakes() {
 		wallStake.move_voltage(6000);
 	} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
 		wallStake.move_voltage(-10000);
-
 	} else if (isResetting == false) {
 		wallStake.brake();
 	}
@@ -32,8 +32,7 @@ void refreshWallstakes() {
 void resetWallstakes() {
 	pros::Task([] {
 		isResetting = true;
-		antiJamToggle = false;
-		while ((wallStakeRotationSensor.get_angle() / 100) < 47) {
+		while ((wallStakeRotationSensor.get_angle() / 100) < 35) {
 			wallStake.move_voltage(8000);
 		}
 		wallStake.brake();
