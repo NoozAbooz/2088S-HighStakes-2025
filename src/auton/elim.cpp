5d05a4efe0aac8d62e45112e1ff04c513811647a
Brandon Kim <105524304+Sir-Encoded@users.noreply.github.com>
2025-02-18 21:32:32 -0700

    origin

--- src/auton/elim.cpp
+++ src/auton/elim.cpp
@@ -9,7 +9,7 @@ void ring_rush() {
     chassis.moveToPoint(-2.5, 45.751, 1250);
     intake.move_voltage(12000);
     leftDoinkerPiston.set_value(true);
-    pros::delay(1380);
+    pros::delay(1450);
     chassis.moveToPoint(0.837, 17.534, 1250, {.forwards = false, .maxSpeed = 100});
     intake.move_voltage(0);
     pros::delay(960);
@@ -28,7 +28,7 @@ void ring_rush() {
     chassis.moveToPoint(14.945, 28.287, 1450, {.forwards = false, .maxSpeed = 80}, false);
 
     // move to 2 ring stack
-    chassis.moveToPoint(-23.934, 37.051, 1400, {.maxSpeed = 70});
+    chassis.moveToPoint(-23.934, 37.051, 1500, {.maxSpeed = 60});
     pros::delay(1600);
     chassis.turnToHeading(200, 500);
     // chassis.moveToPoint(-14, 25, 1000, {.forwards = false});
@@ -39,7 +39,7 @@ void ring_rush() {
 
     // corner clear
     rightDoinkerPiston.set_value(true);
-    chassis.turnToHeading(113.5, 900, {.maxSpeed = 70});
+    chassis.turnToHeading(111.5, 900, {.maxSpeed = 70});
     pros::delay(250);
     rightDoinkerPiston.set_value(false);
     pros::delay(800);
